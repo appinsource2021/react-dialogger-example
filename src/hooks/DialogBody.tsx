ae7b0bc3442220d79112bce3c74ad079f2d45c4f
suleymantopaloglu <developer@appinsource.eu>
2025-12-23 16:25:04 +0100

    Expamle Updated

--- src/hooks/DialogBody.tsx
+++ src/hooks/DialogBody.tsx
@@ -5,32 +5,4 @@ import {IDialogApiDef} from "react-dialogger";
 const DialogBody: React.FC<{dialog: IDialogApiDef}> = ({dialog}) => {
     return <div>Ola SC {dialog?.values?.name} </div>
 }
-
 export {DialogBody};
-
-// const initialValues = {
-//     // ...dialog.formikProps.values,
-//     ...dialog.values.order
-// }
-//
-// const formikProps = useFormik({
-//     // enableReinitialize: true,
-//     initialValues: {
-//         name: 'Suleyman'
-//     },
-//     onSubmit: (values, formikHelpers) => {
-//
-//     },
-// });
-
-// console.log('Dialog_values', dialog.values);
-
-
-// dialog.formikProps = formikProps
-
-// dialog.formikProps = useFormik({
-//     initialValues: {name: 'Suleyman'},
-//     onSubmit:(values, formikHelpers) => {
-//
-//     }
-// });
