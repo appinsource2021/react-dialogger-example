aa29f3ffe133932b6f71b43299e183e04658563a
suleymantopaloglu <developer@appinsource.eu>
2025-12-23 16:24:27 +0100

    Expamle Updated

--- src/hooks/DialogBody.tsx
+++ src/hooks/DialogBody.tsx
@@ -2,11 +2,11 @@ import React, {JSX} from "react";
 import {useFormik} from "formik";
 import {IDialogApiDef} from "react-dialogger";
 
-const ProjectOrderDialogBody: React.FC<{dialog: IDialogApiDef}> = ({dialog}) => {
+const DialogBody: React.FC<{dialog: IDialogApiDef}> = ({dialog}) => {
     return <div>Ola SC {dialog?.values?.name} </div>
 }
 
-export {ProjectOrderDialogBody};
+export {DialogBody};
 
 // const initialValues = {
 //     // ...dialog.formikProps.values,
